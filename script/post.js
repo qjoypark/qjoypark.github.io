6f32b9f8524df9be31532b1ae2d3c7d0885bbac8
qjoypark <qjoypark@gmail.com>
2024-07-16 08:15:52 +0800

    Update sytles

---
+++ script/post.js
@@ -0,0 +1,23 @@
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("show");
+}
+
+// 关闭菜单当点击菜单项时
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.menu a').forEach(item => {
+        item.addEventListener('click', () => {
+            var menu = document.getElementById("menu");
+            menu.classList.remove("show");
+        });
+    });
+
+    // 关闭菜单当点击页面其他地方时
+    document.addEventListener('click', (event) => {
+        var menu = document.getElementById("menu");
+        var menuIcon = document.querySelector('.menu-icon');
+        if (!menu.contains(event.target) && !menuIcon.contains(event.target)) {
+            menu.classList.remove("show");
+        }
+    });
+});
